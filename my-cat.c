d2b18f609a181f1f2301e18f783f68fc21cbf4dc
GenesisN <gensijay@gmail.com>
2018-09-19 22:25:28 +0000

    Added my-cat

---
+++ my-cat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+	if (argc < 2){
+		exit(0);
+	}
+	for (int i=1; i<argc; i++){
+		FILE* fp = fopen(argv[i], "r");
+		if (fp == NULL){
+			printf("cannot open file");
+			return 0;	
+		}
+		int j;
+		while ((j = getc(fp)) !=EOF){
+			putchar(j);
+		}
+		fclose(fp);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
