25b4e0b4fcfe64fbce98fc7ccf29ac18dc2bdbdc
GenesisN <gensijay@gmail.com>
2018-09-20 12:21:40 +0000

    Updated my-cat and my-grep

--- Project1/my-cat.c
+++ Project1/my-cat.c
@@ -2,17 +2,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+// check is there are up to 2 arguments. If not, exit
 int main(int argc, char** argv) {
 	if (argc < 2){
 		exit(0);
 	}
+//check if the file provided exists in the directory. If not, print error message
 	for (int i=1; i<argc; i++){
 		FILE* fp = fopen(argv[i], "r");
 		if (fp == NULL){
 			printf("cannot open file");
 			return 0;	
 		}
+//if the file contains anyline, print them out in order; if not print nothing
 		int j;
 		while ((j = getc(fp)) !=EOF){
 			putchar(j);
--- Project1/my-grep.c
+++ Project1/my-grep.c
@@ -4,18 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char** argv) {
+
+//check if there are up to 3 arguments (executable output, word,file). If not then exit
 	if (argc < 3){
 		exit(0);
 	}
-	
+	// open the file and check. If there is no such file in the directory, print error message
 	for (int i=2; i<argc; i++){
 		FILE* fp = fopen(argv[i], "r");
 		if (fp == NULL){
 			printf("cannot open file");
 			return 0;	
 		}
-		
+	// if the file contains anyline,check if it contains the phrase in argument 1.
+	// if yes, then print the line
 		char * line = NULL;
 		size_t len = 0;
 		ssize_t read;
